ccb451c85cb0fba576d59d5a8b6bf10c691ecb0a
jiangbo0143 <40519599+jiangbo0143@users.noreply.github.com>
2024-07-23 10:14:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'b9459b72-59a6-47de-aafe-35d05f79f1ee';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
